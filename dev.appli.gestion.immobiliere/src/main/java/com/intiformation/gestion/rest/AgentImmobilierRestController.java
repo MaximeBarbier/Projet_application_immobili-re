bb5b268876d5e25e718d6ca7b1a885244cf01756
INTI <INTI@192.168.1.26>
2019-01-29 18:38:13 +0100

    Ajout web services rest

---
+++ dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/rest/AgentImmobilierRestController.java
@@ -0,0 +1,93 @@
+package com.intiformation.gestion.rest;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.intiformation.gestion.entity.Agent;
+import com.intiformation.gestion.entity.Proprietaire;
+import com.intiformation.gestion.metier.IAgenceMetier;
+
+@RestController
+@RequestMapping("/agentImmobilierWs")
+
+public class AgentImmobilierRestController {
+
+	/**
+	 * déclaration et injection du ws 
+	 */
+	@Autowired
+    IAgenceMetier agenceMetier;
+	//setter du service
+
+	public IAgenceMetier getAgenceMetier() {
+		return agenceMetier;
+	}
+
+	public void setAgenceMetier(IAgenceMetier agenceMetier) {
+		this.agenceMetier = agenceMetier;
+	}
+	/*_______________________________________   Methodes_____________________________________________*/
+	/**
+	 * getAllClients
+	 */
+	
+	@RequestMapping(value ="/listeAgents/{agentByBienImmobilier}", method = RequestMethod.GET)
+	
+	public List<Agent>getAllAgents(@PathVariable("agentByBienImmobilier")int id){
+		return agenceMetier.getListAgentByBienImmobilier(id);
+	}//fin getAllClients
+	
+	/**
+	 * getClientsById
+	 * @return
+	 */
+	@RequestMapping(value="/listeAgents/{agentById}",method=RequestMethod.GET)
+	public Agent getAgentById (@PathVariable("agentById")int id) {
+		return agenceMetier.getAgentbyId(id);	
+		
+	}//getClasseStdById
+	
+	/**
+	 * addClasseStd
+	 */
+    @RequestMapping(value = "/createAgent", method = RequestMethod.POST)
+	public void addAgent(@RequestBody Agent agent ) {
+		agenceMetier.ajouterAI(agent);
+		
+		
+	}//fin addClient
+	
+	/**
+	 * updateClasseStd
+	 */
+
+    @RequestMapping(value="/listeAgents/{agentById}",method = RequestMethod.PUT)
+    public void updateAgent (@RequestBody Agent agent) {
+    	
+    	agenceMetier.modifierAI(agent);
+		
+    	
+    }//fin de modifierClient
+
+    
+    /**
+     * deleteClient
+     */
+    @RequestMapping(value="/listeAgents/{agentById}",method = RequestMethod.DELETE)
+    public void deleteAgent(@PathVariable("agentById")int id){
+    	
+    agenceMetier.supprimerAI(id);
+    }//fin deleteClient
+    
+
+
+}//end class
+	
+
+
---
+++ dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/rest/ClientRestController.java
@@ -0,0 +1,101 @@
+package com.intiformation.gestion.rest;
+
+import com.intiformation.gestion.metier.IAgenceMetier;
+import com.intiformation.gestion.entity.Client;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientWs")
+
+public class ClientRestController {
+	
+	/**
+	 * déclaration et injection du ws 
+	 */
+	@Autowired
+    IAgenceMetier agenceMetier;
+	//setter du service
+
+	public IAgenceMetier getAgenceMetier() {
+		return agenceMetier;
+	}
+
+	public void setAgenceMetier(IAgenceMetier agenceMetier) {
+		this.agenceMetier = agenceMetier;
+	}
+	/*_______________________________________   Methodes_____________________________________________*/
+	/**
+	 * getAllClients
+	 */
+	
+	@RequestMapping(value ="/listeClient", method = RequestMethod.GET)
+	
+	public List<Client>getAllClients(){
+		return agenceMetier.listClients();
+	}//fin getAllClients
+	
+	/**
+	 * getClientsById
+	 * @return
+	 */
+	@RequestMapping(value="/listeClient/{clientId}",method=RequestMethod.GET)
+	public Client getClientById (@PathVariable("clientId")int id) {
+		agenceMetier.getClientbyId(id);
+		
+		             
+		
+	
+		return agenceMetier.getClientbyId(id);
+		
+	}//getClasseStdById
+	
+	/**
+	 * addClasseStd
+	 */
+    @RequestMapping(value = "/createClient", method = RequestMethod.POST)
+	public void addClient(@RequestBody Client client  ) {
+		agenceMetier.ajouterClient(client);
+		
+		
+	}//fin addClient
+	
+	/**
+	 * updateClasseStd
+	 */
+
+    @RequestMapping(value="/listeClient/{clientId}",method = RequestMethod.PUT)
+    public void updateClient (@RequestBody Client client) {
+    	
+    	agenceMetier.modifierClient(client);
+		
+    	
+    }//fin de modifierClient
+
+    
+    
+    /**
+     * deleteClient
+     */
+    @RequestMapping(value="/listeClient/{clientId}",method = RequestMethod.DELETE)
+    public void deleteClient(@PathVariable("clientId")int id){
+    	
+    agenceMetier.supprimerClient(id);
+    }//fin deleteClient
+    
+
+
+}//end class
+
+	
+
+
