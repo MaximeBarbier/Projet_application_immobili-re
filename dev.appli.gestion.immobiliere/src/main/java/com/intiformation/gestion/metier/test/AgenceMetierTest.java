8600f7f4e93b8c1fb656e5834162580a50d0e6d0
tina lamrous <lamroustina@gmail.com>
2019-01-29 16:53:08 +0100

    Test

---
+++ dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/metier/test/AgenceMetierTest.java
@@ -0,0 +1,18 @@
+package com.intiformation.gestion.metier.test;
+
+import com.intiformation.gestion.metier.AgenceMetier;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+public class AgenceMetierTest {
+	
+	private AgenceMetier agencemetier;
+	
+	@Before
+	public void setUp() {
+		
+	
+	}
+
+}
