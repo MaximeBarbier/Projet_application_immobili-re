136a7e03b7cda6ab424ce96267e86c9489b202b7
tina lamrous <lamroustina@gmail.com>
2019-01-29 13:11:02 +0100

    ajout repository

--- dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/dao/AgenceDAO.java
+++ dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/dao/AgenceDAO.java
@@ -6,6 +6,10 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
+import org.springframework.stereotype.Repository;
+
+
+@Repository
 public class AgenceDAO implements IAgenceDAO {
 
 	//Entity manager pour utiliser les méthodes jpa
