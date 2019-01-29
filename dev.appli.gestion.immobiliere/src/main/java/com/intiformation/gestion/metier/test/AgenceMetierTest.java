13a48b6a58c5fec5d2439cd6826d0d6eff7513f4
INTI <INTI@192.168.1.26>
2019-01-29 18:38:39 +0100

    Tests unitaires

--- dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/metier/test/AgenceMetierTest.java
+++ dev.appli.gestion.immobiliere/src/main/java/com/intiformation/gestion/metier/test/AgenceMetierTest.java
@@ -5,6 +5,8 @@ import com.intiformation.gestion.metier.AgenceMetier;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
@@ -14,9 +16,14 @@ public class AgenceMetierTest {
 	
 	private AgenceMetier agencemetier;
 	private Proprietaire p;
+	int id = p.getId();
+	
+	
 	
 	@Before
 	public void setUp() {
+		
+		System.out.println("> Avant chaque cas de test");
 		
 		agencemetier = new AgenceMetier();
 	
@@ -25,11 +32,15 @@ public class AgenceMetierTest {
 	@After
 	public void tearDown() {
 		
+		System.out.println("\t> Après chaque cas de test \n");
+		
 		agencemetier=null;
 	}
 	
 	@BeforeClass
 	public static void setUpBeforeClass() {
+		
+		System.out.println("\t>Avant le 1er cas de test \n");
 		
 	}
 	
@@ -37,6 +48,19 @@ public class AgenceMetierTest {
 	@AfterClass
 	public static void tearDownAfterClass() {
 		
+		System.out.println("\t>Après le dernier cas de test \n");
+	}
+	
+	
+	
+	@Test
+	public void testAjoutProprietaire() {
+		
+		System.out.println("\t\t > Cas de test : ajout  d'un propriétaire");
+		
+		p = new Proprietaire(1, "Claude François", "Au paradis", "0654878956");
+		
+		assertNotNull(agencemetier.ajouterProprietaire(p));
 	}
 	
 	@Test
@@ -44,22 +68,45 @@ public class AgenceMetierTest {
 		
 		System.out.println("\t\t > Cas de test : méthode listePropriétaire");
 		
-		assertNotNull(agencemetier.listProprietaires());
+		List<Proprietaire> listeprop = agencemetier.listProprietaires();
+		
+		assertNotNull(listeprop);
 		
 	}
 	
 	@Test
-	public void testAjoutProprietaire() {
-		
-		System.out.println("\t\t > Cas de test : ajout  d'un propriétaire");
-		
-		assertNotNull(agencemetier.ajouterProprietaire(p));
-	}
-	
 	public void testGetProprietaireById() {
 		
 		System.out.println("\t\t > Cas de test : get un propriétaire par son id");
 		
-		asser
+		Proprietaire prop = agencemetier.getProprietairebyId(1);
+		
+		assertNotNull(prop);
+		
 	}
+	
+	@Test
+	public void testModifProp() {
+		
+		System.out.println("\t\t > Cas de test : modifier un propriétaire");
+		
+		Proprietaire prop = agencemetier.getProprietairebyId(1);
+		Proprietaire prop2 = agencemetier.modifierProprietaire(prop);
+		
+		assertEquals(prop, prop2);
+	}
+	
+	@Test
+	public void testSuppressionProp() {
+		
+		System.out.println("\t\t > Cas de test : supprimer un propriétaire");
+		
+		agencemetier.supprimerProprietaire(1);
+		assertNull(agencemetier.getProprietairebyId(1));
+		
+	}
+	
+	
+	
+	
 }
